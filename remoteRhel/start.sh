9313aaf1267cffdc0f48979eff5c25fe02f93c25
douglas.shoemaker <douglas.shoemaker@mantech.com>
2024-10-28 14:28:51 -0400

    Finally got it work, at least with RDP

--- remoteRhel/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-image=remote-rhel
-
-eval "$(podman build -t $image .)"
-
-# Ports are vnc and web based respectively
-eval "$(podman run --name $image -p 3389:3389 -p 8888:80 $image)"
