a24a60fe476581454d6bf1100c289100954cc12a
Douglas Shoemaker <dougshmkr@gmail.com>
2024-10-28 14:38:23 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ eval "podman network create -d bridge main"
 for dir in $dirs; do
   # Create a docker-compose command with the -f option pointing to the YAML file in the current directory
   eval "podman compose -f $dir/docker-compose.yaml up -d"
+  # In the event you have "self-signed cert" issues, try running as docker run with `--volume /etc/pki/ca-trust:/etc/pki/ca-trust`
 done
