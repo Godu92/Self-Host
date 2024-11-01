7e51449ee63ad91369d63ac6efe1aa1f8fccc68f
douglas.shoemaker <douglas.shoemaker@mantech.com>
2024-11-01 09:29:05 -0400

    Adding the ability to ignore starting certain folders Also, being safe by not tracking bookmarks

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
 
-dirs=$(ls -d */)
+dirs=$(ls -d ./*)
+
+# Define the list of excluded directories
+EXCLUDED_DIRS=("adminer" "appsmith" "directus" "remoteRhel" "testing")
 
 eval "podman network create -d bridge main"
 
 # Loop through each directory
 for dir in $dirs; do
-  # Create a docker-compose command with the -f option pointing to the YAML file in the current directory
-  eval "podman compose -f $dir/docker-compose.yaml up -d"
+  excluded_dirs_str=$(printf "%s " "${EXCLUDED_DIRS[@]}")
+  if [[ ! ${excluded_dirs_str} =~ ${dir} ]]; then
+    # Create a docker-compose command with the -f option pointing to the YAML file in the current directory
+    eval "podman compose -f $dir/docker-compose.yaml up -d"
   # In the event you have "self-signed cert" issues, try running as docker run with `--volume /etc/pki/ca-trust:/etc/pki/ca-trust`
+  fi
 done
