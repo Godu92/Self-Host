dc872854c1c18d49e11323c7739f4ccd2f504511
douglas.shoemaker <douglas.shoemaker@mantech.com>
2024-11-15 11:37:00 -0500

    Continuing work on script

--- run2compose.py
+++ run2compose.py
@@ -1,67 +1,146 @@
+import argparse
+
 import yaml
 
 
-def transform(cmd: str) -> str:
+def create_args() -> argparse.Namespace:
+    """Method to setup argument parsing
+
+    Returns:
+        (argparse.Namespace): The arguments to pass along
+    """
+    parser = argparse.ArgumentParser(
+        prog="run2compose",
+        description="Process `docker run` command into compose",
+        usage="%(prog)s.py docker_cmd [-h] [-f [file]] [-n [traefik_network]] [-d [domain]]",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    # The version
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1")
+
+    # The docker run arg
+    parser.add_argument(
+        "cmd",
+        metavar="cmd",
+        dest="docker_cmd",
+        type=str,
+        help="the `docker run` command",
+    )
+
+    # If you want a different file name
+    parser.add_argument(
+        "-f",
+        "--file",
+        metavar="file",
+        dest="file",
+        nargs="?",
+        default="docker-compose.yaml",
+        type=str,
+        help="the file name",
+    )
+
+    # If you want a different network name
+    parser.add_argument(
+        "-n",
+        "--network",
+        metavar="network",
+        nargs="?",
+        default="proxy",
+        type=str,
+        help="the network name",
+    )
+
+    # If you want a different host name
+    parser.add_argument(
+        "-d",
+        "--domain",
+        metavar="domain",
+        nargs="?",
+        default="localhost",
+        type=str,
+        help="the domain name; ie `name.localhost`",
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    return args
+
+
+def transform(args) -> str:
     """Transforms cmd into compose yaml
 
     Args:
-      cmd (str) = the docker run command to convert
+      args (str) = the docker run command to convert
 
     Returns:
       (str) = the template string of a compose file
     """
-    template: str = "services:\n"
+    # Define the docker compose JSON-like object
+    template: dict = {"services": {}}
+    simple_name: str = None
 
-    pieces: list[str] = cmd.split(" ")
+    pieces: list[str] = args.docker_cmd.split(" ")
 
     # We want just the name without the image tag, but only if there is one
     if ":" in pieces[-1]:
         name: str = pieces[-1].split(":")[0]
     else:
         name = pieces[-1]
-    template += f"\t{name.split("/")[1]}:\n"
+    if "/" in name:
+        simple_name: str = name.split("/")[1].replace("-", "")
+    else:
+        simple_name = name
 
     image: str = pieces[-1]
-    template += f"\timage: {image}\n"
-    template += "\trestart: unless-stopped\n"
-    template += f"\tcontainer_name: {name}\n"
+    template["services"][f"{simple_name}"] = {}
+    template["services"][f"{simple_name}"]["image"] = f"{image}"
+    template["services"][f"{simple_name}"]["restart"] = "unless-stopped"
+    template["services"][f"{simple_name}"]["container_name"] = f"{simple_name}"
+    template["services"][f"{simple_name}"]["ports"] = []
+    template["services"][f"{simple_name}"]["environment"] = []
+    template["services"][f"{simple_name}"]["volumes"] = []
+    template["services"][f"{simple_name}"]["labels"] = []
 
     # TODO: Likely should be lists in the event there's more than one
+
     for i, item in enumerate(pieces):
-        # if ports are provided
-        if "-p" in item:
-            # get item after flag and split
-            ports: list[str] = pieces[i + 1].split(":")
-            outer_port: str = ports[0]
-            inner_port: str = ports[1]
-            template += "\t#ports:\n"
-            template += f"\t#\t- {outer_port}:{inner_port}\n"
+        match item:
 
-    # if volumes are provided
-    if "-v" in item:
-        # get item after flag and split
-        volumes: list[str] = pieces[i + 1].split(":")
-        outer_path: str = volumes[0]
-        inner_path: str = volumes[1]
-        template += "\t#volumes:\n"
-        template += f"\t#\t- {outer_path}:{inner_path}\n"
+            # if ports are provided
+            case "-p":
+                # get item after flag and split
+                ports: list[str] = pieces[i + 1].split(":")
+                inner_port: str = ports[1]
+                template["services"][f"{simple_name}"]["ports"].append(
+                    f"{':'.join(ports)}"
+                )
 
-    # if environments are provided
-    if "-e" in item:
-        # get item after flag and split
-        envs: list[str] = pieces[i + 1]
-        template += "\t#environment:\n"
-        template += f"\t#\t{envs}\n"
+            # if volumes are provided
+            case "-v":
+                # get item after flag and split
+                volumes: list[str] = pieces[i + 1]
+                template["services"][f"{simple_name}"]["volumes"].append(f"{volumes}")
 
-    template += "\tlabels:\n"
-    template += "\t\t- traefik.enable=true\n"
+            # if environments are provided
+            case "-e":
+                # get item after flag and split
+                envs: list[str] = pieces[i + 1]
+                template["services"][f"{simple_name}"]["environment"].append(f"{envs}")
 
-    host: str = "localhost"
-    template += f"\t\t- traefik.http.routers.{name}.rule=Host(`{name}.{host}`)\n"
-    template += f"\t\t- traefik.http.routers.{name}.entrypoints=http\n"
-    template += (
-        f"\t\t- traefik.http.services.{name}.loadbalancer.server.port={inner_port}\n"
+    labels: list[str] = ["traefik.enable=true"]
+
+    host: str = args.domain
+
+    labels.append(
+        f"traefik.http.routers.{simple_name}.rule=Host(`{simple_name}.{host}`)"
     )
+    labels.append(f"traefik.http.routers.{simple_name}.entrypoints=http")
+    labels.append(
+        f"traefik.http.services.{simple_name}.loadbalancer.server.port={inner_port}"
+    )
+    template["services"][f"{simple_name}"]["labels"] = labels
 
     return template
 
@@ -71,11 +150,30 @@ def main():
     produce compose file with `traefik` tags based on image name,
     also create folder based on image name and add to root compose
     """
-    cmd: str = (
-        "docker run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password kasmweb/only-office:1.16.0"
-    )
-    output: str = transform(cmd)
-    yaml_output: str = yaml.dump(output, default_flow_style=False)
+    # Create the parser
+    args: argparse.Namespace = create_args()
+
+    # cmd: str = (
+    #     "docker run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password kasmweb/only-office:1.16.0"
+    # )
+
+    # Adding networks to bottom of file
+    ## TODO: get traefik network
+    traefik_network: str = args.network
+    networks: dict = {
+        "networks": {
+            "default": {
+                "name": f"{traefik_network}",
+                "external": "true",
+            }
+        }
+    }
+
+    output: str = transform(args)
+    yaml_output: str = yaml.dump(output, default_flow_style=True, indent=4)
+    print(yaml_output)
+
+    yaml_output = yaml.dump(networks, default_flow_style=True, indent=4)
     print(yaml_output)
 
 
